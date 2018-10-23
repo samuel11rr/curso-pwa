b08d18a6765aaf762c556b1fa66c8cf2c9655938
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-10-23 16:00:03 -0500

    PRIMEROS EJERCICIOS CON CACHE

---
+++ 04-cache-offline/sw.js
@@ -0,0 +1,33 @@
+self.addEventListener('fetch', event => {
+
+  // const offlineResp = new Response(`
+  //       Bienvenido, necesitas internet.
+  //     `);
+
+  // const offlineResp = new Response(`
+  //   <!DOCTYPE html>
+  //   <html lang="en">
+  //   <head>
+  //       <meta charset="UTF-8">
+  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  //       <title>Mi PWA offline</title>
+  //
+  //   </head>
+  //   <body class="container p-3">
+  //     <h1>Modo offline</h1>
+  //   </body>
+  //   </html>
+  //     `, {
+  //       headers: {
+  //         'Content-Type': 'text/html'
+  //       }
+  //     });
+
+  const offlineResp = fetch('pages/offline.html');
+
+  const resp = fetch( event.request )
+                  .catch( () => offlineResp);
+
+  event.respondWith( resp );
+});
