31a85910aa43973732e6293d44a65a0b280a45c3
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-10-26 12:00:24 -0500

    GUARDAR Y LEER REGISTROS

--- 08-pouchdb-todo/js/app.js
+++ 08-pouchdb-todo/js/app.js
@@ -11,6 +11,11 @@
   var db = new PouchDB('todos');
   var remoteCouch = false;
 
+  db.changes({
+    since: 'now',
+    live: true
+  }).on('change', showTodos);
+
   // We have to create a new todo document and enter it in the database
   function addTodo(text) {
     var todo = {
@@ -33,6 +38,14 @@
 
   // Show the current list of todos by reading them from the database
   function showTodos() {
+    // db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+    //   redrawTodosUI(doc.rows);
+    // });
+
+    db.allDocs({include_docs: true, descending: true}).then(doc => {
+      console.log(doc);
+      redrawTodosUI(doc.rows);
+    });
   }
 
   function checkboxChanged(todo, event) {
